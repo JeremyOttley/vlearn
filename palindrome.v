259903f7bf51468de3186d78ae7da1c4e04fe3c7
Jeremy Ottley <Gazaunga@gmail.com>
2023-08-08 15:01:22 -0400

    Update palindrome.v

--- palindrome.v
+++ palindrome.v
@@ -1,9 +1,10 @@
-
+import os
+	
 fn palindrome(word string) bool {
 	return word.split("").reverse() == word.split("")
 }
 
 fn main() {
-	println(palindrome("Jeremy"))
+	println(palindrome(os.args[1]))
 }
 
