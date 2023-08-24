41bbd772cfb81aa7981ae756e9b2bb9ccb7067dc
Jeremy Ottley <Gazaunga@gmail.com>
2023-08-23 20:44:37 -0400

    Update users.v

--- users.v
+++ users.v
@@ -1,10 +1,10 @@
 module main
 
-//import net.http
+import net.http
 import json
 //import mewzax.chalk
 
-//const user_api = 'https://jsonplaceholder.typicode.com/users'
+const user_api = 'https://jsonplaceholder.typicode.com/users'
 
 struct Geo {
 pub:
@@ -41,36 +41,16 @@ pub:
 }
 
 fn main() {
-  
-json_data := '{
-    "id": 1,
-    "name": "Leanne Graham",
-    "username": "Bret",
-    "email": "Sincere@april.biz",
-    "address": {
-      "street": "Kulas Light",
-      "suite": "Apt. 556",
-      "city": "Gwenborough",
-      "zipcode": "92998-3874",
-      "geo": {
-        "lat": "-37.3159",
-        "lng": "81.1496"
-      }
-    },
-    "phone": "1-770-736-8031 x56442",
-    "website": "hildegard.org",
-    "company": {
-      "name": "Romaguera-Crona",
-      "catchPhrase": "Multi-layered client-server neural-net",
-      "bs": "harness real-time e-markets"
-    }
-  }'
+json_data := http.get_text(user_api)
 
-  mut zipcodes := json.decode(User, json_data) or {
+
+  mut zipcodes := json.decode([]User, json_data) or {
      eprintln('Failed to parse json, error: ${err}')
      return
   }
-  
-  println(zipcodes.address.zipcode)
+
+for user in zipcodes {
+  println("Your zipcode is: ${user.address.zipcode}")
+}
 
 }
