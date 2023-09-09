ddfd5b9611697475b7393d844068dbd3e5339e3a
Jeremy Ottley <Gazaunga@gmail.com>
2023-09-09 08:37:48 -0400

    Update sumType.v

--- sumType.v
+++ sumType.v
@@ -6,8 +6,19 @@ struct Resolved {}
 
 type Status = Null | Alias | Prime | Update | Resolved
 
-sum := Status(Alias{})
-assert sum.type_name() == 'Alias'
-println(sum)
+fn check_status(sum Status) string {
+	return match true {
+		sum.type_name() == 'Null' { sum.type_name() }
+		sum.type_name() == 'Alias' { sum.type_name() }
+		sum.type_name() == 'Prime' { sum.type_name() }
+		sum.type_name() == 'Update' { sum.type_name() }
+		sum.type_name() == 'Resolved' { sum.type_name() }
+		else { 'Error: failed to fetch status' }
+	}
+}
 
-
+fn main() {
+	sum := Status(Alias{})
+	assert sum.type_name() == 'Alias'
+	println(check_status(sum))
+}
