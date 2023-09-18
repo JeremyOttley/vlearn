98024dc0c7c62f87716df3c9d5c97ea6d6e955fa
Jeremy Ottley <Gazaunga@gmail.com>
2023-09-18 12:43:27 -0400

    Update isbn.v

--- isbn.v
+++ isbn.v
@@ -3,10 +3,14 @@ const isbn = "978-1-4780-2737-9"
 fn normalize(isbn string) string {
 	mut s := isbn
 	for s.contains('-') {
-    	s = s.replace('-','')
-    }
+    		s = s.replace('-','')
+	}
   return s 
 }
 
-assert(normalize(isbn) == "9781478027379")
-println(normalize(isbn))
+fn main() {
+
+	assert(normalize(isbn) == "9781478027379")
+	
+	println(normalize(isbn))
+}
