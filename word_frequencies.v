4625b8284c3e5adf690c159259ce49dfaae1f075
Jeremy Ottley <Gazaunga@gmail.com>
2023-10-07 09:40:56 -0400

    Update word_frequencies.v

--- word_frequencies.v
+++ word_frequencies.v
@@ -1,29 +1,32 @@
-import regex
+//import regex
+import arrays
 
 // count frequencies of unique words
 
 // print them out with their frequencies, ordered most frequent first
 
-fn string_to_array(s string) []string {
-	return s.runes().map(it.str())
-}
+//fn string_to_array(s string) []string {
+//	return s.runes().map(it.str())
+//}
 
-fn normalize(isbn string) string {
-	mut s := isbn
+//fn normalize(isbn string) string {
+//	mut s := isbn
 	
-	mut re_non_digit := regex.regex_opt(r'\W+') or { panic(err) }
-	s = re_non_digit.replace(s, '')
+//	mut re_non_digit := regex.regex_opt(r'\W+') or { panic(err) }
+//	s = re_non_digit.replace(s, '')
 	
-  return s 
-}
+//  return s 
+//}
 
 input := "The foo the foo the  defenestration the"
 
 // strint -> array
 
 input_array := input.split(" ").filter(it != '').map(it.to_lower())
-//input_array.join("")
-println(input_array)
+
+	for k,v in arrays.map_of_counts(input_array) {
+		println("${k}: ${v}\n")
+	}
 
 
 
