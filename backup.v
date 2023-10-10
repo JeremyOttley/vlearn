3f03db95661a022d789c3e46e93e9f33bcae57fd
Jeremy Ottley <Gazaunga@gmail.com>
2023-10-10 16:20:33 -0400

    Update backup.v

--- backup.v
+++ backup.v
@@ -1,3 +1,4 @@
+import mewzax.chalk
 import os
 import time
 
@@ -8,5 +9,9 @@ fn main() {
 	source := os.args[1]
 	backup_time := n.custom_format("YYYY-MMMM-DD")
 	destination := backup_time + "-" + source
-	os.cp(source, destination)!
+	os.cp(source, destination) or {
+     eprintln('Failed to copy file or directory, error: ${err}')
+     return
+  }
+	println(chalk.fg("${source} has been backed up to ${destination}", 'green'))
 }
