d73e9ae9ab937695e2252637d8a0e991faa76b08
Jeremy Ottley <Gazaunga@gmail.com>
2023-10-10 16:31:57 -0400

    Update backup.v

--- backup.v
+++ backup.v
@@ -1,17 +1,22 @@
 import mewzax.chalk
 import os
 import time
+import log
 
 const n = time.now()
 
 
 fn main() {
+
 	source := os.args[1]
+
 	backup_time := n.custom_format("YYYY-MMMM-DD")
 	destination := backup_time + "-" + source
+
 	os.cp(source, destination) or {
-     eprintln('Failed to copy file or directory, error: ${err}')
-     return
-  }
-	println(chalk.fg("${source} has been backed up to ${destination}", 'green'))
+		log.error(chalk.fg("Failed to back up ${source} to ${destination}", 'red'))
+     	return
+  	}
+
+	log.info(chalk.fg("${source} has been backed up to ${destination}", 'green'))
 }
