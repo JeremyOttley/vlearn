dd06d9aa5b119b2f47e90d38de23a84ddcc72677
Jeremy Ottley <Gazaunga@gmail.com>
2023-10-16 10:01:22 -0400

    Create invariant.v

---
+++ invariant.v
@@ -0,0 +1,16 @@
+fn insertion_point(xs []i32, x i32) i32 {
+  mut lo := 0
+  mut hi := xs.len
+  //while loop
+  for hi > lo {
+    mid := lo + (hi - lo) / 2
+    if x < xs[mid] {
+      lo = mid
+    } else {
+      hi = mid
+    }
+  }
+return lo
+}
+
+assert(insertion_point([1,2,3,4,5], 5) == 0)
