f60e4ba142d338ee7c06b76846e7c67e3a441800
Jeremy Ottley <Gazaunga@gmail.com>
2024-01-29 16:47:51 -0500

    Update product_update.v

--- product_update.v
+++ product_update.v
@@ -1,12 +1,11 @@
   const (
-    isbn = "978-1-4780-2737-9"
     prefix = "A"
     suffix = "E"
-    ppv = "PPV_BV""
+    ppv = "PPV_BV"
   )
 
 fn main() {
-  mut i := isbn
-  pd := prefix + i.split("-")[3] + suffix
-  ppd := ppv + prefix + i.split("-")[3] + suffix
+  mut isbn := "978-1-4780-2737-9"
+  pd := prefix + isbn.split("-")[3] + suffix
+  ppd := ppv + prefix + isbn.split("-")[3] + suffix
 }
