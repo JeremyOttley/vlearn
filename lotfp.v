d486c6566922f74cbe4e61273791426e67198171
Jeremy Ottley <Gazaunga@gmail.com>
2024-02-04 15:44:46 -0500

    Update lotfp.v

--- lotfp.v
+++ lotfp.v
@@ -14,14 +14,27 @@ json_data := json2.raw_decode(api_request(url)!)!.as_map()
 //equipment := json_data['equipment']!.arr()
 //appearance := json_data['appaearance']!.str()
 
-// Implement structs properly
+// Implement with anonymous structs
 
 struct Root {
 	thac9 int @[omitempty]
 	spell []string @[omitempty]
 	ac int @[omitempty]
-	saves Saves @[omitempty]
-	attr Attr @[omitempty]
+	saves struct {
+		wands int @[omitempty]
+		breath int @[omitempty]
+		stone int @[omitempty]
+		magic int @[omitempty]
+		poison int @[omitempty]
+	} @[omitempty]
+	attr struct {
+		d_e_x string @[json: "DEX"; omitempty]
+		c_h_a string @[json: "CHA"; omitempty]
+		i_n_t string @[json: "INT"; omitempty]
+		w_i_s string @[json: "WIS"; omitempty]
+		s_t_r string @[json: "STR"; omitempty]
+		c_o_n string @[json: "CON"; omitempty]
+	} @[omitempty]
 	hp int @[omitempty]
 	to_hit [][] @[omitempty]
 	appearance string @[omitempty]
@@ -33,23 +46,6 @@ struct Root {
 	attributes [][] @[omitempty]
 	notes []Any @[omitempty]
 	personality string @[omitempty]
-}
-
-struct Saves {
-	wands int @[omitempty]
-	breath int @[omitempty]
-	stone int @[omitempty]
-	magic int @[omitempty]
-	poison int @[omitempty]
-}
-
-struct Attr {
-	d_e_x string @[json: "DEX"; omitempty]
-	c_h_a string @[json: "CHA"; omitempty]
-	i_n_t string @[json: "INT"; omitempty]
-	w_i_s string @[json: "WIS"; omitempty]
-	s_t_r string @[json: "STR"; omitempty]
-	c_o_n string @[json: "CON"; omitempty]
 }
 
 struct Any{}
