9db001a22c846a8c53eaf43b52d331907c8b83b5
Jeremy Ottley <Gazaunga@gmail.com>
2025-02-19 15:58:55 -0500

    Update palindrome.v

--- palindrome.v
+++ palindrome.v
@@ -1,11 +1,15 @@
 import os
 	
-fn palindrome(word string) bool {
+fn palindrome(word string) ?bool {
+	if word == "" {
+		return none
+	}
 	return word.split("").reverse() == word.split("")
 }
 
 fn main() {
 	word := os.input("Enter a some text: ")
-	println(palindrome(word))
+	println(palindrome(word)?)
 }
+
 
